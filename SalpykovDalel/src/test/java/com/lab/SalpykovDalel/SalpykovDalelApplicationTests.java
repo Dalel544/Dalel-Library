891e2a101de5811fedcaaecadc194cde44c9283f
Dalel544 <salpykovdalel9@gmail.com>
2026-05-19 21:55:03 +0600

    Refactor codebase and cleanup project structure

---
+++ SalpykovDalel/src/test/java/com/lab/SalpykovDalel/SalpykovDalelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.SalpykovDalel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalpykovDalelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
