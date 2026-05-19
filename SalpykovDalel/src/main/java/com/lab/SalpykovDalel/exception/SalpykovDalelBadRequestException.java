7b583e39a40257d510ecdd2486829b6bee68e763
Dalel544 <salpykovdalel9@gmail.com>
2026-05-19 21:53:33 +0600

    Add global exception handling

---
+++ SalpykovDalel/src/main/java/com/lab/SalpykovDalel/exception/SalpykovDalelBadRequestException.java
@@ -0,0 +1,7 @@
+package com.lab.SalpykovDalel.exception;
+
+public class SalpykovDalelBadRequestException extends RuntimeException {
+    public SalpykovDalelBadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ SalpykovDalel/src/main/java/com/lab/SalpykovDalel/exception/SalpykovDalelResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.lab.SalpykovDalel.exception;
+
+public class SalpykovDalelResourceNotFoundException extends RuntimeException {
+    public SalpykovDalelResourceNotFoundException(String message) {
+        super(message);
+    }
+}
