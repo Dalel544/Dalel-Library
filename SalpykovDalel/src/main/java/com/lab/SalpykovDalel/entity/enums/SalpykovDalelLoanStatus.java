f73a05e5e2d3a2f4786dd9de44068bf978ed098c
Dalel544 <salpykovdalel9@gmail.com>
2026-05-19 21:43:27 +0600

    Add loan entity and status enum

---
+++ SalpykovDalel/src/main/java/com/lab/SalpykovDalel/entity/enums/SalpykovDalelLoanStatus.java
@@ -0,0 +1,8 @@
+package com.lab.SalpykovDalel.entity.enums;
+
+public enum SalpykovDalelLoanStatus {
+    ACTIVE,      // книга на руках
+    RETURNED,    // книга возвращена
+    OVERDUE,     // просрочено
+    LOST         // книга потеряна
+}
